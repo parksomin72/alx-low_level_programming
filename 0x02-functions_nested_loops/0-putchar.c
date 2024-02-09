1f4859e26ae7845dfa5e5b18fe4b99c1f65d4aa1
parksomin72 <fatimazahraealawi@gmail.com>
2024-02-09 11:10:41 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Etry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* Declaration of the _putchar function */
+void _putchar(void);
+
+int main(void)
+{
+    _putchar();
+    printf("\n");
+    return 0;
+}
---
+++ 0x02-functions_nested_loops/putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * _putchar print _putchar
+ *
+ */
+void _putchar(void)
+{
+    /* Print _putchar */
+    printf("_putchar");
+}
