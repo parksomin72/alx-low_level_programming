64afbcfd0e27a4d8741e17e0598db0490fca217a
parksomin72 <fatimazahraealawi@gmail.com>
2024-02-09 21:31:27 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Etry point
  *
  * Return: Always 0 (Success)
  */
-/* Declaration of the _putchar function */
-void _putchar(void);
 
 int main(void)
 {
-    _putchar();
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
     printf("\n");
     return 0;
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#define _MAIN_H_
+#ifndef _MAIN_H_
+int _putchar (char c);
+
+#endif
