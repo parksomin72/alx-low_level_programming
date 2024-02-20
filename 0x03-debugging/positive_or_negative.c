c5ced29c13c8017ba1a00cd46dcf7c5150ec4261
parksomin72 <fatimazahraealawi@gmail.com>
2024-02-20 04:19:00 -0800

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0(Success)
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+}
