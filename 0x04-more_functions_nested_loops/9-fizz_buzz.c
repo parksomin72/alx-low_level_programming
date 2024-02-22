6e87cb07ec3ad3b9f2e32eaab8be4206747b6866
parksomin72 <fatimazahraealawi@gmail.com>
2024-02-21 16:24:34 -0800

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			if ((i % 5) == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else
+			{
+			printf("Fizz ");
+			}
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+		printf("%d ", i);
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
