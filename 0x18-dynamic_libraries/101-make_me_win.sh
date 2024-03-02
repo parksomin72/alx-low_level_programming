c437a86b6e5feee8d461e22243e029fecb4316d7
parksomin72 <fatimazahraealawi@gmail.com>
2024-03-02 10:58:51 -0800

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ..  https://github.com/parksomin72/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
+wget -P .. https://raw.githubusercontent.com/parksomin72/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
 export LD_PRELOAD="$PWD/../libhack.so"
--- 0x18-dynamic_libraries/hack.c
+++ 0x18-dynamic_libraries/hack.c
@@ -4,7 +4,7 @@
 
 int printf(const char *format, ...)
 {
-	write(1, "9 8 10 24 75+9\n", 15);
-	write(1, "Congratulation, you win the Jackpot!\n", 38);
-	return (EXIT_SUCCESS);
+	write(1, "9 8 10 24 75 +9\n", 17);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
+	exit (EXIT_SUCCESS);
 }
