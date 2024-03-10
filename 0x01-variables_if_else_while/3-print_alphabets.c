0de8bc080b9c5752bd84cc214fb23da66b86ec4c
parksomin72 <fatimazahraealawi@gmail.com>
2024-03-10 16:29:35 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (Seccuss)
+ */
+int main(void)
+{
+	char letters = 'a';
+	char alpha = 'A';
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		putchar(letters);
+	}
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+		
+}
