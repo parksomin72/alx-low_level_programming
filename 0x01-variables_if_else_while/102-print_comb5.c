b353aaad8c968539d3e0f8a3da6ef702849cc947
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-06 11:18:56 -0700

    new update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,19 +21,16 @@ int main(void)
 		x = i / 10;
 		y = i % 10;
 
-		if (x == 0 && y == 0)
-		{
-		putchar(z + '0');
-		putchar(k + '0');
-		putchar(' ');
 		putchar(x + '0');
 		putchar(y + '0');
+		putchar(' ');
+		putchar(z + '0');
+		putchar(k + '0');
 		
 		if (j != 99)
 		{
 			putchar(',');
 			putchar(' ');
-		}
 		}
 	}
 	}
