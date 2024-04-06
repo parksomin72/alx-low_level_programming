345c96651ce45d50f41c6c7cbb0710ec0decb7c4
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-06 11:11:53 -0700

    new update

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, x, y, j;
+
+	for (j = 0; j <100; j++)
+	{
+		int z, k;
+		z = j / 10;
+		k = j % 10;
+	for (i = 0; i < 100; i++)
+	{
+		x = i / 10;
+		y = i % 10;
+
+		putchar(z + '0');
+		putchar(k + '0');
+		putchar(' ');
+		putchar(x + '0');
+		putchar(y + '0');
+		
+		if (i != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
