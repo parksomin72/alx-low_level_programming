53ce8ae79fe7ab468fd87a420371c48727c4f3e2
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-06 11:17:08 -0700

    new update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,26 +9,31 @@ int main(void)
 {
 	int i, x, y, j;
 
-	for (j = 0; j <100; j++)
+	for (j = 0; j < 100; j++)
 	{
 		int z, k;
+
 		z = j / 10;
 		k = j % 10;
+
 	for (i = 0; i < 100; i++)
 	{
 		x = i / 10;
 		y = i % 10;
 
+		if (x == 0 && y == 0)
+		{
 		putchar(z + '0');
 		putchar(k + '0');
 		putchar(' ');
 		putchar(x + '0');
 		putchar(y + '0');
 		
-		if (i != 99)
+		if (j != 99)
 		{
 			putchar(',');
 			putchar(' ');
+		}
 		}
 	}
 	}
