192f9ce4841d991887e55c058fb629b492c62506
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-06 19:03:56 -0700

    _putchar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _putchar - prints one charactere
- * @c: char that print
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int _putchar(char c)
 {
 	putchar(c);
+	/*return(write(1, &c, 1));*/
 	return (0);
 }
