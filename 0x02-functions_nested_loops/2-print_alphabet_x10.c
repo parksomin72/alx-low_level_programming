ea8132561d97b7424138395cd2a7ac88f629ee53
parksomin72 <fatimazahraealawi@gmail.com>
2024-06-01 04:29:24 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,14 @@
 void print_alphabet_x10(void)
 {
 	int N;
-        char alpha;
+	char alpha;
 
 	for (N = 0; N < 10; N++)
 	{
-        for (alpha = 'a'; alpha <= 'z'; alpha++)
-        {
-                _putchar(alpha);
-        }
-        _putchar('\n');
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
 	}
 }
