0caa28b63c1d3de95c7c0ffa062ed580afe45019
parksomin72 <fatimazahraealawi@gmail.com>
2024-06-02 03:11:37 -0700

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-/**
- * reset_to_98 - updates the value it points to to 98.
- * @n: int as parameter and updates the value it points.
- */
-void reset_to_98(int *n)
-{
-	*n = 98;
-}
