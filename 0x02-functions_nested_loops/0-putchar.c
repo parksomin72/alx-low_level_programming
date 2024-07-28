5923af4bd24c5d222727d71c1b93f2f8d3314aa0
parksomin72 <fatimazahraealawi@gmail.com>
2024-07-28 01:25:43 -0700

    remove

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * main -  prints _putchar.
  *
- * Return: always 0 (Success)
  */
-int main(void)
-{
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-	return (0);
-}
