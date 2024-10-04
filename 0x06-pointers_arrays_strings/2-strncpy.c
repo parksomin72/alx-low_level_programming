07cba61812d2d1874fdf31d87aa5f073ac6145c2
parksomin72 <fatimazahraealawi@gmail.com>
2024-10-04 14:41:50 -0700

    add

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n && dest[i])
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
