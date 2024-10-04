ca2bc321e97dcd01b094adc319bb9013086e1fd9
parksomin72 <fatimazahraealawi@gmail.com>
2024-10-04 01:04:46 -0700

    add

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,11 +12,17 @@ int _atoi(char *s)
 
 	while (s[i])
 	{
-		if (s[i] == '-')
-			sign *= - 1;
+		if (s[i] == '-' || s[i] == '+')
+		{
+			if (s[i] == '-')
+				sign *= -1;
+		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			result = result * 10 + (s[i] - 48);
+
+			if (result * sign == -2147483648)
+				return (-2147483648);
 			start = 1;
 		}
 		else if (start)
