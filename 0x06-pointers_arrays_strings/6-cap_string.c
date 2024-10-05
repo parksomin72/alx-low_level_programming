10452aee6c6879b038744214bbf7b13417f74e0a
parksomin72 <fatimazahraealawi@gmail.com>
2024-10-05 12:22:09 -0700

    add

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
@@ -9,20 +8,19 @@
 char *cap_string(char *str)
 {
 	int i = 0;
-	
+
 	if (str[i] >= 'a' && str[i] <= 'z')
 		str[i] -= 32;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' ||
-		str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' ||
-		str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+			|| str[i] == '.' || str[i] == ',' || str[i] == ';' ||
+			str[i] == '(' || str[i] == ')' || str[i] == '!' ||
+			str[i] == '?' || str[i] == '"' || str[i] == '{' || str[i] == '}')
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-			{
 				str[i + 1] -= 32;
-			}
 		}
 		i++;
 	}
