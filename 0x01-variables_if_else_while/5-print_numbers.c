539e211acbe0184c848e1383bbed783403f9922c
parksomin72 <fatimazhraealawi@gmail.com>
2024-11-06 11:54:13 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,4 +3,16 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
+int main(void)
+{
+	int num = 0;
 
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	printf("\n");
+
+	return (0);
+}
