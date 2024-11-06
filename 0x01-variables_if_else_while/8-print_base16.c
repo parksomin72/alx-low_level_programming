202ce125a9c3037ba08b26a128f512cf5973bdff
parksomin72 <fatimazhraealawi@gmail.com>
2024-11-06 13:21:31 -0800

    Smile in the mirror

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,13 +5,4 @@
  */
 int main(void)
 {
-	int a = 0;
-	char alpha = 'a';
-
-	for (; a < 10; a++)
-		putchar(a + 48);
-	for (; alpha <= 'f'; alpha++)
-		putchar(alpha);
-	putchar('\n');
-	return (0);
 }
