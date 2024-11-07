2e82a671e850a60fde2133ebf44423a95c869e1f
parksomin72 <fatimazhraealawi@gmail.com>
2024-11-06 16:31:37 -0800

    update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,20 +3,3 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	int a = 0;
-
-	while (a < 10)
-	{
-		putchar(a + 48);
-		if (a != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		a++;
-	}
-	putchar('\n');
-	return (0);
-}
