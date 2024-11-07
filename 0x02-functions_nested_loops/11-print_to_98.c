1bee762739bc778e91d727d6bc7a0712aa3c7fe2
parksomin72 <fatimazhraealawi@gmail.com>
2024-11-07 11:53:03 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,26 +5,4 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
-	{
-		while (n >= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-				printf(", ");
-			n--;
-		}
-		printf("\n");
-	}
-	else if (n < 98)
-	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-				printf(", ");
-			n++;
-		}
-		printf("\n");
-	}
 }
