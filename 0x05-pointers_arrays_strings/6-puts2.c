b991173c63b03ddeec09b6ce085799f9f0432eff
parksomin72 <fatimazhraealawi@gmail.com>
2024-11-07 17:57:50 -0800

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,9 +7,13 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i, len;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	len = 0;
+	while (str[len])
+		len++;
+
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
 	}
