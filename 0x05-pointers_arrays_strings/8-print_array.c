73a45b4865107a6502504578ad04ede900aaf06d
ParkSoMinE <parksomin051096@gmail.com>
2024-11-12 13:05:07 -0800

    add

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,25 @@
 #include "main.h"
 #include <stdio.h>
+
+void print_number(int *a)
+{
+	/*int len, arr[10];
+
+	len = 0;*/
+
+	if (*a >= 10 && *a < 100)
+	{
+		putchar((*a / 10) + 48);
+		putchar((*a % 10) + 48);
+	}
+	
+
+	/*while (len > 0)
+	{
+		putchar(arr[len] + 48);
+		len--;
+	}*/
+}
 /**
  * print_array - a function that prints n elements of an array
  * @a: array name
@@ -8,14 +28,18 @@
  */
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	while (i < n)
 	{
-		printf("%d", a[i]);
+		print_number(&a[i]);
 		if (i != n - 1)
-			printf(", ");
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		i++;
 	}
-	printf("\n");
+	putchar('\n');
 }
