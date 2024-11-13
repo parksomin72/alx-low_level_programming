c648eff05ef4d43e5a1f2fbb4c7550bd46cd9afd
ParkSoMinE <parksomin051096@gmail.com>
2024-11-13 03:15:03 -0800

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 /**
  * _atoi - converts a string to an integer
@@ -7,28 +8,39 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 0, len = 0, result = 0, start = 0, digit = 0;
+	int sign, result, is_digit;
 
-	while (s[len] != '\0')
-		len++;
-	while (i < len && start == 0)
+	sign = 1;
+	result = 0;
+	is_digit = 0;
+
+	while (*s)
 	{
-		if (s[i] == '-')
-			++sign;
-		if (s[i] >= '0' && s[i] <= '9')
+		/*while (*s == ' ' || *s == '\t' || *s == '\v' || *s == '\f'
+			|| *s == '\r' || *s == '\n')
+			s++;*/
+		if (*s == '+' || *s == '-')
 		{
-			digit = s[i] - '0';
-			if (sign % 2)
-				digit = -digit;
-			result = result * 10 + digit;
-			start = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			start = 0;
+			if (*s == '-')
+				sign *= -1;
 		}
-		i++;
+		else if (*s >= '0' && *s <= '9')
+		{
+			is_digit = 1;
+			break;
+		}
+
+		s++;
 	}
-	if (start == 0)
-		return (0);
-	return (result);
+
+	if (is_digit)
+	{
+		while (*s >= '0' && *s <= '9')
+		{
+			result = result * 10 + (*s - 48);
+			s++;
+		}
+	}
+
+	return (sign * result);
 }
