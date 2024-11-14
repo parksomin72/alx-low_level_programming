ca9b1b58dd3b321e906fd6071ad408818fe24c33
parksomin72 <fatimazahraealawi@gmail.com>
2024-11-13 20:37:52 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,17 +9,4 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
-
-	while (src[i] && i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	while (i < n && dest[i])
-	{
-		dest[i] = '\0';
-		i++;
-	}
-	return (dest);
 }
