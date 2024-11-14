dc10ef3dd266280dceefeb748f2859b4ac0ac44c
parksomin72 <fatimazahraealawi@gmail.com>
2024-11-14 00:51:54 -0800

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,17 +7,22 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	if (str[i] >= 'a' && str[i] <= 'z')
+	{
 		str[i] -= 32;
+		i++;
+	}
 
 	while (str[i])
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
-			|| str[i] == '.' || str[i] == ',' || str[i] == ';' ||
-			str[i] == '(' || str[i] == ')' || str[i] == '!' ||
-			str[i] == '?' || str[i] == '"' || str[i] == '{' || str[i] == '}')
+		if (str[i] == ',' || str[i] == ';' || str[i] == '.'
+			|| str[i] == '!' || str[i] == '?' || str[i] == '"'
+			|| str[i] == '(' || str[i] == ' ' || str[i] == '\t'
+			|| str[i] == '\n' || str[i] == ')' || str[i] == '{'
+			|| str[i] == '}')
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] -= 32;
