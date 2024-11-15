38223b7b6ee08e448fcc836b2af412e1cf1b009b
parksomin72 <fatimazahraealawi@gmail.com>
2024-11-15 00:20:51 -0800

    It is the addition of strangeness to beauty that constitutes the romantic character in art

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,67 @@
+#include "main.h"
+int len(char *str)
+{
+        int len, i;
+
+        for (i = 0, len = 0; str[i]; i++)
+                len++;
+        return (len);
+}
+
+int size(char *n1, char *n2)
+{
+	int len_n1, len_n2;
+
+	len_n1 = len(n1);
+	len_n2 = len(n2);
+	if (len_n1 > len_n2)
+		return (len_n1);
+	else
+		return (len_n2);
+}
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int max_size, carry, sum, i, j;
+
+	max_size = size(n1, n2);
+
+	if (size_r <= max_size + 1)
+		return (0);
+
+	i = len(n1) - 1;
+	j = len(n2) - 1;
+	carry = 0;
+	r[max_size + 1] = '\0';
+
+	while (i >= 0 || j >= 0 || carry > 0)
+	{
+		sum = carry;
+		if (i >= 0)
+			sum += n1[i--] - 48;
+		if (j >= 0)
+			sum += n2[j--] - 48;
+		carry = sum / 10;
+		r[max_size--] = (sum % 10) + 48;
+
+		if (max_size < 0 && (i >= 0 || j >= 0 || carry > 0))
+			return (0);
+	}
+
+	if (max_size >= 0)
+	{
+		for (i = 0; r[max_size + 1 - i]; i++)
+			r[i] = r[max_size + 1 - i];
+		r[i] = '\0';
+	}
+
+	return (r);
+	
+}
