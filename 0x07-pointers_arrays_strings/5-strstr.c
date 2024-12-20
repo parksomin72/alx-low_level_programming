24c9795f7d1f41d3ded2904fe7f7fb59bb311e27
parksomin72 <fatimazahraealawi@gmail.com>
2024-12-20 04:36:58 -0800

    Update

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,7 +4,6 @@
  *           in the string `haystack`.
  * @haystack: The string to search in.
  * @needle: The substring to search for.
- * 
  * Return: A pointer to the beginning of the located substring,
  *         or NULL if the substring is not found.
  */
