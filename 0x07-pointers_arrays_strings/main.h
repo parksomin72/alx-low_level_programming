7201478c507f8545bb6f4bf5354ab2372f7efcee
parksomin72 <fatimazahraealawi@gmail.com>
2024-12-19 16:47:24 -0800

    add

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+
+/**** Standard Labaries ****/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+/****** Functions Prototypes *****/
+char *_memset(char *s, char b, unsigned int n);
+void simple_print_buffer(char *buffer, unsigned int size);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+
+#endif /* _MAIN_H_ */
