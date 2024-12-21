fbbda1fd800ba296e55874b63b1c3467911a9acb
parksomin72 <fatimazahraealawi@gmail.com>
2024-12-21 02:15:18 -0800

    It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,11 @@
 #include <unistd.h>
-
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char **argv)
 {
 	if (argc > 0)
