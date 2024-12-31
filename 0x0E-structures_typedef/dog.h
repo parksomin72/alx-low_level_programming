5d4f7614537e557ceb3412d3b89696214a946f06
parksomin72 <fatimazahraealawi@gmail.com>
2024-12-31 01:41:21 -0800

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog -a dog's basic info
+ * @name: First member
+ * @age: Seconde member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
